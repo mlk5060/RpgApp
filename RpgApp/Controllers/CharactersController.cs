71ce0ce81875daa7c2c3844d19249566ce36a23b
Martyn Lloyd-Kelly <martynlloydkelly@gmail.com>
2016-12-21 17:51:35 +0000

    Basic character creation

---
+++ RpgApp/Controllers/CharactersController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RpgApp;
+using RpgApp.Models;
+
+namespace RpgApp.Controllers
+{
+    public class CharactersController : Controller
+    {
+        private RpgContext db = new RpgContext();
+
+        // GET: Characters
+        public ActionResult Index()
+        {
+            var characters = db.Characters.Include(c => c.Gender1).Include(c => c.Race1);
+            return View(characters.ToList());
+        }
+
+        // GET: Characters/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Character character = db.Characters.Find(id);
+            if (character == null)
+            {
+                return HttpNotFound();
+            }
+            return View(character);
+        }
+
+        // GET: Characters/Create
+        public ActionResult Create()
+        {
+            ViewBag.Gender = new SelectList(db.Genders, "Id", "Text");
+            ViewBag.Race = new SelectList(db.Races, "Id", "Name");
+            return View();
+        }
+
+        // POST: Characters/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name,Age,Gender,Lvl,Race", Exclude = "Id,Created")] Character character)
+        {
+            character.Created = System.DateTime.Now;
+            if (ModelState.IsValid)
+            {
+                db.Characters.Add(character);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Gender = new SelectList(db.Genders, "Id", "Text", character.Gender);
+            ViewBag.Race = new SelectList(db.Races, "Id", "Name", character.Race);
+            return View(character);
+        }
+
+        // GET: Characters/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Character character = db.Characters.Find(id);
+            if (character == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Gender = new SelectList(db.Genders, "Id", "Text", character.Gender);
+            ViewBag.Race = new SelectList(db.Races, "Id", "Name", character.Race);
+            return View(character);
+        }
+
+        // POST: Characters/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Age,Gender,Lvl,Race,Created")] Character character)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(character).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Gender = new SelectList(db.Genders, "Id", "Text", character.Gender);
+            ViewBag.Race = new SelectList(db.Races, "Id", "Name", character.Race);
+            return View(character);
+        }
+
+        // GET: Characters/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Character character = db.Characters.Find(id);
+            if (character == null)
+            {
+                return HttpNotFound();
+            }
+            return View(character);
+        }
+
+        // POST: Characters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Character character = db.Characters.Find(id);
+            db.Characters.Remove(character);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
